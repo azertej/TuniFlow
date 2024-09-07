9a05a21fc89fdf54b2083a9971c836fe2bba251f
azertej <azer.tej.2001@gmail.com>
2024-09-07 13:21:48 +0100

    add needed folders and include auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 import React from "react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,12 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`antialiased`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home () {
-  return (
-    <div className="flex justify-center items-center">
-      <p className="h1-bold"> Hello World</p>
-    </div>
-  );
-}
